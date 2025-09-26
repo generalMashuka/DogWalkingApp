991ee52c88792ccac808ff925e445d92981d8e53
Masha J <112469464+generalMashuka@users.noreply.github.com>
2025-09-26 15:49:22 +0200

    init

--- src/App.js
+++ src/App.js
@@ -5,20 +5,36 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+              <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Dog-up
         </a>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This is my pet project to help people walk their dogs.
+        </p>
+        <div className='.App-button-container'>
+        <p>click here <button className='App-button'>dog-up</button> to book a walk  </p>
+        </div>
+
       </header>
+    <div>
+    <footer className="App-footer">
+    <div className='App-contacts'>
+      <p>Contact us at &#9786; jurgensonmk@google.com</p>
     </div>
+    <div className='App-social-media'> 
+    Made with &hearts; by <a href="https://github.com/generalMashuka"> Masha</a> 
+    </div>
+      
+    </footer>
+    </div>
+    </div>
+    
   );
 }
 
